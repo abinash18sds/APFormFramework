da8d014bb50eb5c714ee037c35b0df7e63cc3273
sundewappteam <abinash@sundewsolutions.com>
2024-12-19 11:40:05 +0530

    Initial Commit

---
+++ APFormFramework/APFormFramework.h
@@ -0,0 +1,18 @@
+//
+//  APFormFramework.h
+//  APFormFramework
+//
+//  Created by Sundew on 19/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for APFormFramework.
+FOUNDATION_EXPORT double APFormFrameworkVersionNumber;
+
+//! Project version string for APFormFramework.
+FOUNDATION_EXPORT const unsigned char APFormFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APFormFramework/PublicHeader.h>
+
+
---
+++ APFormFrameworkTests/APFormFrameworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  APFormFrameworkTests.swift
+//  APFormFrameworkTests
+//
+//  Created by Sundew on 19/12/24.
+//
+
+import Testing
+@testable import APFormFramework
+
+struct APFormFrameworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
